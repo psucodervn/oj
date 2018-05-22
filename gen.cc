fbb8bcd6a096fcd1c5434b3ad8e48454fbd4a0af
Hung Le <hungle.info@gmail.com>
2018-05-22 11:11:00 +0700

    Remove temp

--- gen.cc
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-#include "testlib.h"
-using namespace std;
-
-int main(int argc, char* argv[]) {
-  registerGen(argc, argv, 1);
-  ios::sync_with_stdio(false); cin.tie(nullptr);
-  return 0;
-}
