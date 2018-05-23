cac7cb07d80a794dac3c8ed7da2961489bf21d48
Hung Le <hungle.info@gmail.com>
2018-05-24 00:23:36 +0700

    Change

--- codeforces/985/A/test.sh
+++ codeforces/985/A/test.sh
@@ -2,10 +2,37 @@
 if ! g++ -g -std=c++14 -Wall $DBG main.cc; then
     exit
 fi
-cd tests
 INPUT_EXT=in
 OUTPUT_EXT=out
 MY_EXT=res
+
+id=0
+begin=1
+exts=("in" "out")
+ex_id=0
+mkdir -p tests
+while IFS='' read -r line || [[ -n "$line" ]]; do
+  if [ "$line" == "-=-=-=" ]; then
+    let ex_id=0;
+    let begin=1;
+  elif [ -z "$line" ]; then
+    let begin=1;
+    let ex_id=1;
+  else
+    if [ $begin -eq 1 ]; then
+      if [ $ex_id -eq 0 ]; then
+        let id+=1;
+      fi
+      let begin=0;
+      echo $line > tests/$id.${exts[$ex_id]};
+    else
+      echo $line >> tests/$id.${exts[$ex_id]};
+    fi
+  fi
+done < "tests.in"
+
+cd tests
+
 rm -R *$MY_EXT &>/dev/null
 for test_file in *.$INPUT_EXT
 do
