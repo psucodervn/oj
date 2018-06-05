04df2c7f47144fa404146fa8da9cb73c1e1b72de
Hưng Lê <hunglm@vzota.com.vn>
2018-06-05 12:18:27 +0700

    Update...

--- codeforces/448/D/test.sh
+++ codeforces/448/D/test.sh
@@ -1,29 +1,4 @@
-{{ define "main.cc" }}/*
-  Task: {{ .Name }}
-  Site: {{ .Site }}
-  Link: {{ .URL }}
-*/
-#include <bits/stdc++.h>
-using namespace std;
-
-void err(istream_iterator<string> it) { cerr << endl; }
-template<typename T, typename... Args> void err(istream_iterator<string> it, T a, Args... args) { cerr << *it << " = " << a << ", "; err(++it, args...); }
-#define db(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
-#define dba(ar) { cerr << #ar << " = ["; for (auto x : ar) cerr << " " << x; cerr << "]" << endl; }
-
-int main(int argc, char* argv[]) {
-  ios::sync_with_stdio(false); cin.tie(nullptr);
-  {{- if ne .Input "stdin" }}
-  freopen("{{ .Input }}", "r", stdin);
-  {{- end }}
-  {{- if ne .Output "stdout" }}
-  freopen("{{ .Output }}", "r", stdout);
-  {{- end }}
-  return 0;
-}
-{{ end }}
-
-{{- define "test.sh" }}#!/bin/bash
+#!/bin/bash
 if ! g++ -std=c++14 -Wall main.cc -o main; then
     exit
 fi
@@ -88,4 +63,3 @@ do
         fi
     fi
 done
-{{- end }}
